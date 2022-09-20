46ee608de2cf36ff94ccf8ceff004f7428b8d6e5
thaoklg8 <you@example.comthaovtt19406@st.uel.edu.vn git config --global user.name thaoklg8git config --global user.pass 97452684Aa@git config --global user.pass 97452684Aa@ git config --global user.email you@example.comthaovtt19406@st.uel.edu.vn>
2022-09-20 23:25:32 +0700

    create and update time

---
+++ webcmd/src/main/java/com/webcmd/constant/CrossOriginConstant.java
@@ -0,0 +1,6 @@
+package com.webcmd.constant;
+
+public class CrossOriginConstant {
+	public static final String REACT_ORIGIN = "http://27.74.244.150:3000/";
+	public static final String REACT_ORIGIN_LOCAL = "http://localhost:3000/";
+}
---
+++ webcmd/src/main/java/com/webcmd/entity/ResponseObject.java
@@ -0,0 +1,17 @@
+package com.webcmd.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseObject {
+	String status;
+	String message;
+	Object data;
+
+}
